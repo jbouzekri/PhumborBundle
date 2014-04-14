75214d06649256ce57bb6e5a7c6716ffe9dad8a5
jbouzekri <jonathan.bouzekri@gmail.com>
2014-04-14 22:02:24 +0200

    Bundle class

---
+++ JbPhumborBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jb\Bundle\PhumborBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Description of JbPhumborBundle
+ *
+ * @author jobou
+ */
+class JbPhumborBundle extends Bundle
+{
+}
