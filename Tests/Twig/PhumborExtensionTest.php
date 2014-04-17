5d2124ed76d212a298a53e9ec620d4cd64bade41
jbouzekri <jonathan.bouzekri@gmail.com>
2014-04-17 21:35:07 +0200

    add twig function

---
+++ Tests/Twig/PhumborExtensionTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Jb\Bundle\PhumborBundle\Tests\Twig;
+
+use Jb\Bundle\PhumborBundle\Twig\PhumborExtension;
+use Jb\Bundle\PhumborBundle\Transformer\BaseTransformer;
+use Thumbor\Url\BuilderFactory;
+
+/**
+ * Description of PhumborExtensionTest
+ *
+ * @author jobou
+ */
+class PhumborExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Jb\Bundle\PhumborBundle\Twig\PhumborExtension
+     */
+    private $extension;
+
+    /**
+     * @var \Thumbor\Url\BuilderFactory
+     */
+    private $factory;
+
+    /**
+     * SetUp
+     */
+    public function setUp()
+    {
+        $this->factory = new BuilderFactory('http://localhost', '123456789');
+        $transformer = new BaseTransformer(
+            $this->factory,
+            array(
+                'width_50' => array(
+                    'resize' => array('width'=>50, 'height'=>0)
+                )
+            )
+        );
+        $this->extension = new PhumborExtension($transformer);
+    }
+
+    /**
+     * Test twig getFilters
+     */
+    public function testGetFilters()
+    {
+        $this->assertEquals(count($this->extension->getFilters()), 1);
+    }
+
+    /**
+     * Test twig getName
+     */
+    public function testGetName()
+    {
+        $this->assertEquals($this->extension->getName(), 'phumbor_extension');
+    }
+
+    /**
+     * Test twig get filters
+     */
+    public function testTransform()
+    {
+        $transformedUrl = $this->extension->transform('logo.png', 'width_50');
+        $builtUrl = $this->factory->url('logo.png')->resize(50,0);
+
+        $this->assertEquals($builtUrl, $transformedUrl);
+    }
+}
---
+++ Twig/PhumborExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Jb\Bundle\PhumborBundle\Twig;
+
+use Jb\Bundle\PhumborBundle\Transformer\BaseTransformer;
+
+/**
+ * Description of PhumborExtension
+ *
+ * @author jobou
+ */
+class PhumborExtension extends \Twig_Extension
+{
+    /**
+     * @var \Jb\Bundle\PhumborBundle\Transformer\BaseTransformer
+     */
+    protected $transformer;
+
+    /**
+     * Constructor
+     *
+     * @param \Jb\Bundle\PhumborBundle\Transformer\BaseTransformer $transformer
+     */
+    public function __construct(BaseTransformer $transformer)
+    {
+        $this->transformer = $transformer;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('thumbor', array($this, 'transform')),
+        );
+    }
+
+    /**
+     * Twig thumbor filter
+     *
+     * @param string $orig
+     * @param string $transformation
+     * @param array $overrides
+     *
+     * @return string
+     */
+    public function transform($orig, $transformation = null, $overrides = array())
+    {
+        return $this->transformer->transform($orig, $transformation, $overrides);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'phumbor_extension';
+    }
+}
