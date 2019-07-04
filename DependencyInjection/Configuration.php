0b23c23a9d54dc55c9292953819656918ef10e7a
Zizitto <stelsvitya@gmail.com>
2019-07-04 18:07:48 +0300

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -6,6 +6,7 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\HttpKernel\Kernel;
+
 /**
  * PhumborBundle configuration structure.
  *
